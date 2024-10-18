6956b073eb23ffa08b363cd027531aeefbb95168
Hanie Bp <haniebaratpoor@gmail.com>
2024-10-18 14:41:30 +0330

    change the home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,8 +8,8 @@ const Home = ({start}) => {
         <i className="bi bi-flower3 text-white fs-1"></i>
         <h1 className="text-white mx-0 my-auto">The React Quiz </h1>
       </div>
-      <h3 className="mb-2">welcome to the react quiz!</h3>
-      <p>10 questions to test your React mastery</p>
+      <h3 className="mb-2">welcome to the quiz app!</h3>
+      <p>5 questions to test your  mastery</p>
       <button className="p-2 my-3 btn  rounded text-white" onClick={start}>Let's get started</button>
     </div>
   );
